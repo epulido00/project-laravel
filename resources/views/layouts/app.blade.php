7322bb55078cc26668947a91147a2746f36a00ba
Edwin Pulido <edwin@Edwins-MacBook-Pro.local>
2020-12-01 12:29:21 -0600

    Cambie el titulo nada mas para prueba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titulo</title>
+    <title>Lista de tareas</title>
 </head>
 <body>
     <div style = "width: 100%; height: 50px; background-color: gray;">
